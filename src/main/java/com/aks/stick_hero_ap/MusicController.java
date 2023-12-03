8b583f18a760f1d9b5bef90eb5bb54300330f3c9
Akanksh <akanksh22046@iiitd.ac.in>
2023-12-04 00:54:11 +0530

    Audio Manager to control Global Audio, Music Controller for Controlling music for each scene and Sound interface to use in relevant classes.

---
+++ src/main/java/com/aks/stick_hero_ap/MusicController.java
@@ -0,0 +1,52 @@
+package com.aks.stick_hero_ap;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public class MusicController {
+    private MediaPlayer mediaPlayer;
+    private Media media;
+    private String audioFile;
+
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+    }
+
+    public void setMediaPlayer(MediaPlayer mediaPlayer) {
+        this.mediaPlayer = mediaPlayer;
+    }
+
+    public Media getMedia() {
+        return media;
+    }
+
+    public void setMedia(Media media) {
+        this.media = media;
+    }
+
+    public String getAudioFile() {
+        return audioFile;
+    }
+
+    public void setAudioFile(String audioFile) {
+        this.audioFile = audioFile;
+    }
+
+    public MusicController(String audioFile) {
+        this.audioFile = audioFile;
+        this.media = new Media(audioFile);
+        this.mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE); // Loop the audio
+    }
+
+    public void playAudio() {
+        if (!mediaPlayer.isMute()) {
+            mediaPlayer.play();
+        }
+    }
+
+    public void stopAudio() {
+        mediaPlayer.pause();
+    }
+}
+
---
+++ src/main/java/com/aks/stick_hero_ap/Sound.java
@@ -0,0 +1,6 @@
+package com.aks.stick_hero_ap;
+
+public interface Sound {
+    void initialiseSound();
+    void muteUnmute();
+}
