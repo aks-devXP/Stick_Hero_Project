e1dc878726d1cd7cf0b551973b5aeb67b92fde04
DELL <akshat22051@iiitd.ac.in>
2023-12-12 00:03:19 +0530

    Comments added

--- src/main/java/com/aks/stick_hero_ap/HomeScreen.java
+++ src/main/java/com/aks/stick_hero_ap/HomeScreen.java
@@ -14,6 +14,11 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.atomic.AtomicLong;
 
+
+/*This is the main class of the code which is used to run the game,
+in this code there is a main function and a start function, both of which are mandatory
+to run a javafx program
+ */
 public class HomeScreen extends Application {
     public static void main(String[] args) {
         launch(args);
@@ -21,7 +26,10 @@ public class HomeScreen extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        /*
+        The FXMLLOADER loads the fxml file for the homescreen and then it is used loaded into the scene
+        Then this scene is shown using the stage object.
+         */
         FXMLLoader fxmlLoader = new FXMLLoader(HomeScreen.class.getResource("HomeScreen.fxml"));
         //Parent root=fxmlLoader.load();
         Scene scene = new Scene(fxmlLoader.load());
